da16657810fd25b009de2cf87dfbefe56382eecb
Jabez Roberts <info@jabezroberts.com>
2022-06-15 16:22:58 -0500

    second commit

--- utils/fetchApi.js
+++ utils/fetchApi.js
@@ -5,7 +5,7 @@ export const baseUrl = 'https://bayut.p.rapidapi.com';
 export const fetchApi = async (url) => {
     const { data } = await axios.get((url), {
         headers: {
-            'X-RapidAPI-Key': '8862ee23cemshd82c6f58b7cf841p1e016cjsnb15c871e739e',
+            'X-RapidAPI-Key': '',
             'X-RapidAPI-Host': 'bayut.p.rapidapi.com'
           }
     })
